4f2f0830c788dc7f0be893eb1ac3af70f70d9751
Biel <vgabriel@id.uff.br>
2020-06-29 17:51:08 -0300

    Movimento e modelo 3d do personagem inseridos no jogo

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    //vetores de rotação para definir para onde o personagem irá olhar:
+    Vector3 upOrFront = Vector3.zero;
+    Vector3 right = new Vector3(0, 90, 0);
+    Vector3 downOrBack = new Vector3(0, 180, 0);
+    Vector3 left = new Vector3(0, 270, 0);
+
+    //vetor de direção atual do personagem durante o tempo de jogo:
+    Vector3 currentDirection = Vector3.zero;
+
+    //vetores para serem usados para movimentação em si:
+    Vector3 nextPos, destination, direction;
+
+    float moveSpeed = 5f;
+    float rayLength = 1f;
+
+    bool canMove = false;
+    void Start()
+    {
+        currentDirection = upOrFront;
+        nextPos = Vector3.forward;
+        destination = transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            nextPos = Vector3.forward;
+            currentDirection = upOrFront;
+            canMove = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            nextPos = Vector3.back;
+            currentDirection = downOrBack;
+            canMove = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            nextPos = Vector3.right;
+            currentDirection = right;
+            canMove = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            nextPos = Vector3.left;
+            currentDirection = left;
+            canMove = true;
+        }
+
+        if(Vector3.Distance(destination, transform.position) <= 0.00001f)
+        {
+            transform.localEulerAngles = currentDirection;
+
+            if(canMove)
+            {
+                if(IsValid())
+                {
+                    destination = transform.position + nextPos;
+                    direction = nextPos;
+                    canMove = false;
+                }
+                
+            }
+        }
+    }
+
+    bool IsValid()
+    {
+        Ray myRay = new Ray(transform.position + new Vector3(0, 0.25f, 0), transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
+
+        if(Physics.Raycast(myRay, out hit, rayLength))
+        {
+            if(hit.collider.tag == "Wall")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
